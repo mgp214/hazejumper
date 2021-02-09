83356f5bf5816e19eb9ab4b58c159ae938ff06c5
Matthew Popp <mgp214@gmail.com>
2021-02-08 22:51:48 -0700

    started cutter tool
    #15

---
+++ Assets/_Scripts/Useables/CutterBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutterBehaviour : Useable {
+
+	void Start() {
+		onSelected += Selected;
+		onDeselected += Deselected;
+		CanSwitchOut = true;
+	}
+
+	/// <summary>
+	/// Logic for when this tool is selected from the tool list.
+	/// </summary>
+	private void Selected() {
+		transform.GetChild(0).gameObject.SetActive(true);
+	}
+
+	/// <summary>
+	/// Logic for when this tool is deselected from the tool list.
+	/// </summary>
+	private void Deselected() {
+		transform.GetChild(0).gameObject.SetActive(false);
+	}
+}
